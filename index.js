d9bea1c0d089d7e3bceabc7387eb543a3adff664
Daniel Calvin <arockne2003@yahoo.com>
2021-05-20 22:35:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function iterativeLog(array) {
-  array.forEach((element, index) => console.log(`${index}: ${element}asd`));
+  array.forEach((element, index) => console.log(`${index}: ${element}`));
 }
 
 function iterate(callback) {
