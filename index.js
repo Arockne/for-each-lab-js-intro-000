72549f5216b4319122a943e20920de5e79acaac3
Daniel Calvin <arockne2003@yahoo.com>
2021-05-20 22:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function iterativeLog(array) {
 function iterate(callback) {
   let array = [1, 2, 3];
   array.forEach(callback);
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
 }
